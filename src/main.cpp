b74045f6a7b59681a8130401c00552ceeb3d4e7f
Aryan <aa2522@cam.ac.uk>
2025-08-22 15:49:19 +0100

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,105 @@
 #include <Arduino.h>
+/*!
+  * @file  getGNSS.ino
+  * @copyright Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+  * @license The MIT License (MIT)
+  * @author ZhixinLiu(zhixin.liu@dfrobot.com)
+  * @version V0.1
+  * @date 2022-08-15
+  * @url https://github.com/dfrobot/DFRobot_GNSS
+  */
 
-#include <SoftwareSerial.h>
-
-SoftwareSerial mySerial(11, 10); // RX, TX
-unsigned char data[4] = {};
-float distance;
+#include "DFRobot_GNSS.h"
 
 
-void setup() {
-  Serial.begin(57600);
-  // Print log
-  Serial.println("setup");
+//DFRobot_GNSS_I2C gnss(&Wire ,GNSS_DEVICE_ADDR);
 
-  //Serial.begin(57600);
-  mySerial.begin(9600);
-}
+#ifdef ESP_PLATFORM
+  // ESP32 user hardware uart
+  // RX io16
+  // TX io17
+  DFRobot_GNSS_UART gnss(&Serial2 ,9600);
+#else
+  // Arduino user software uart
+  // RX io10
+  // TX io11
+  SoftwareSerial mySerial(10 ,11);
+  DFRobot_GNSS_UART gnss(&mySerial ,9600);
+#endif
 
-float i=0;
-void loop() {
-  do {
-    for (int i = 0; i < 4; i++)
-    {
-      data[i] = mySerial.read();
-    }
-  } while (mySerial.read() == 0xff);
-
-  mySerial.flush();
-
-  if (data[0] == 0xff)
-  {
-    int sum;
-    sum = (data[0] + data[1] + data[2]) & 0x00FF;
-    if (sum == data[3])
-    {
-      distance = (data[1] << 8) + data[2];
-      if (distance > 260)
-      {
-        Serial.print(">distance:");
-        Serial.print(distance / 10);
-      } else
-      {
-        Serial.println("Below the lower limit");
-      }
-    } else Serial.println("ERROR");
+void setup() 
+{
+  Serial.begin(9600);
+  while(!gnss.begin()){
+    Serial.println("NO Deivces !");
+    delay(1000);
   }
-  delay(50);
+
+  gnss.enablePower();      
+
+/** Set the galaxy to be used
+ *   eGPS              USE gps
+ *   eBeiDou           USE beidou
+ *   eGPS_BeiDou       USE gps + beidou
+ *   eGLONASS          USE glonass
+ *   eGPS_GLONASS      USE gps + glonass
+ *   eBeiDou_GLONASS   USE beidou +glonass
+ *   eGPS_BeiDou_GLONASS USE gps + beidou + glonass
+ */
+  gnss.setGnss(eGPS_BeiDou_GLONASS);
+
+
+  // gnss.setRgbOff();
+  gnss.setRgbOn();
+  // gnss.disablePower();      
 }
+
+void loop()
+{
+  sTim_t utc = gnss.getUTC();
+  sTim_t date = gnss.getDate();
+  sLonLat_t lat = gnss.getLat();
+  sLonLat_t lon = gnss.getLon();
+  double high = gnss.getAlt();
+  uint8_t starUserd = gnss.getNumSatUsed();
+  double sog = gnss.getSog();
+  double cog = gnss.getCog();
+
+  Serial.println("");
+  Serial.print(date.year);
+  Serial.print("/");
+  Serial.print(date.month);
+  Serial.print("/");
+  Serial.print(date.date);
+  Serial.print("/");
+  Serial.print(utc.hour);
+  Serial.print(":");
+  Serial.print(utc.minute);
+  Serial.print(":");
+  Serial.print(utc.second);
+  Serial.println();
+  Serial.println((char)lat.latDirection);
+  Serial.println((char)lon.lonDirection);
+  
+  // Serial.print("lat DDMM.MMMMM = ");
+  // Serial.println(lat.latitude, 5);
+  // Serial.print(" lon DDDMM.MMMMM = ");
+  // Serial.println(lon.lonitude, 5);
+  Serial.print("lat degree = ");
+  Serial.println(lat.latitudeDegree,6);
+  Serial.print("lon degree = ");
+  Serial.println(lon.lonitudeDegree,6);
+
+  Serial.print("star userd = ");
+  Serial.println(starUserd);
+  Serial.print("alt high = ");
+  Serial.println(high);
+  Serial.print("sog =  ");
+  Serial.println(sog);
+  Serial.print("cog = ");
+  Serial.println(cog);
+  Serial.print("gnss mode =  ");
+  Serial.println(gnss.getGnssMode());
+  delay(1000);
+}
+
